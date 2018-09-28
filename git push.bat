d4257584963164c72db533232f34e8ccde1048b2
Muhammadinaam <muhammad.inaam@aurea.com>
2018-09-28 14:42:45 +0500

    h

--- git push.bat
+++ git push.bat
@@ -1,4 +1,5 @@
 git config --global --unset credential.helper
+git config user.name Muhammadinaam
 git remote set-url origin https://Muhammadinaam@github.com/Muhammadinaam/rms/
 git add .
 git commit -m "h"
